908ee76f7ae0bae74f86e5d3560b82bdd1db6c31
anjshnkr <anjshnkr@umich.edu>
2023-08-31 15:06:45 -0400

    First commit

---
+++ _plugins/unscape_cls_html.rb
@@ -0,0 +1,16 @@
+# ref: https://martinopilia.com/posts/2020/02/22/migration.html
+
+# Unescape HTML in the prefix and suffix of elements in the CLS.
+# Required to be able to insert HTML tags within the CLS.
+require 'cgi'
+require 'citeproc/ruby'
+
+class CiteProc::Ruby::Formats::Html
+  def prefix
+    CGI.unescape_html options[:prefix]
+  end
+
+  def suffix
+    CGI.unescape_html options[:suffix]
+  end
+end
